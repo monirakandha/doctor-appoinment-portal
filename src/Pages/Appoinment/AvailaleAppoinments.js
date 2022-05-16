fbacf106aa77852373852a689cce5a87a8fa8b96
monirhosenakanda <monirhosenakanda@gmail.com>
2022-05-17 02:02:34 +0600

    fontend part done

--- src/Pages/Appoinment/AvailaleAppoinments.js
+++ src/Pages/Appoinment/AvailaleAppoinments.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import Service from './Service';
+import BookingModal from './BookingModal';
 const AvailaleAppoinments = ({date}) => {
     const [services , setServices] = useState([]);
+    const [treatment , setTreatment] = useState( null);
+
     useEffect( () => {
-        fetch('service.json')
+        fetch('http://localhost:5000/service')
         .then(res => res.json())
         .then( data => setServices(data))
     }, [])
@@ -14,9 +17,11 @@ const AvailaleAppoinments = ({date}) => {
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
               {
                   services.map(service => <Service key={service._id} service={service}
+                    setTreatment = {setTreatment}
                   ></Service>)
               }
           </div>
+          {treatment && <BookingModal setTreatment = {setTreatment} date={date} treatment={treatment}></BookingModal>}
         </div>
     );
 };
--- src/Pages/Appoinment/Service.js
+++ src/Pages/Appoinment/Service.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Service = ({service}) => {
+const Service = ({service , setTreatment}) => {
     const {name, slots} = service;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
@@ -13,7 +13,9 @@ const Service = ({service}) => {
             </p>
             <p>{slots.length}{slots.length > 1 ? 'spaces' : 'space'} Spaces Lenghth</p>
             <div class="card-actions">
-            <button disabled={slots.length === 0} class="btn btn-primary">Booking Now</button>
+            <label for="booking-modal" disabled={slots.length === 0} 
+            onClick={() => setTreatment (service)} class="btn btn-primary">Booking Now</label>
+
             </div>
         </div>
         </div>
